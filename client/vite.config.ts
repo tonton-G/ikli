35662c22a69df0082c37d4742d603f5bda1ee521
Anthony <83625612+tonton-G@users.noreply.github.com.>
2026-09-01 12:43:48 +0800

    add vite config

---
+++ client/vite.config.ts
@@ -0,0 +1,18 @@
+import { fileURLToPath } from 'node:url';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3001',
+    },
+  },
+});
