75808a6f61f280ef0d9d077a6c02b8b8ffe13bb6
Anthony <83625612+tonton-G@users.noreply.github.com.>
2026-09-10 09:51:08 +0800

    added dynamodb on output

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,3 +2,8 @@ output "account_id" {
   description = "AWS account ID resolved from current credentials"
   value       = data.aws_caller_identity.current.account_id
 }
+
+output "links_table_name" {
+  description = "DynamoDB table backing the link store; set as DYNAMODB_TABLE on instances"
+  value       = aws_dynamodb_table.links.name
+}
