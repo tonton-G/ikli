f7aea39f31f8cff716b57e2a3a3ea6c897c547b7
Anthony <83625612+tonton-G@users.noreply.github.com.>
2026-09-10 10:26:32 +0800

    chore: added default and value

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,7 @@ variable "aws_account_id" {
   description = "AWS account ID this configuration is allowed to target"
   type        = string
 }
+variable "my_ip_cidr" {
+  description = "Workstation IP as a /32, for ALB access during the build"
+  type        = string
+}
