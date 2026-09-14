dedb3afa50d39c43b089f51b67e53fa0b8faf650
Anthony <83625612+tonton-G@users.noreply.github.com.>
2026-09-14 10:37:13 +0800

    chore: added output acm certificate

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,3 +7,12 @@ output "links_table_name" {
   description = "DynamoDB table backing the link store; set as DYNAMODB_TABLE on instances"
   value       = aws_dynamodb_table.links.name
 }
+output "acm_certificate_arn_cloudfront" {
+  description = "ARN of the CloudFront-facing ACM certificate (us-east-1)"
+  value       = aws_acm_certificate.cloudfront.arn
+}
+
+output "acm_certificate_arn_alb" {
+  description = "ARN of the ALB-facing ACM certificate (ap-southeast-1)"
+  value       = aws_acm_certificate.alb.arn
+}
