25c83bb9ca7f0b9cf80201220d01d2eaf06c0c47
Anthony <83625612+tonton-G@users.noreply.github.com.>
2026-09-21 11:46:57 +0800

    chore: changes default cache behavior and added ordered cache behavior on cloudfront terraform

--- infra/cloudfront.tf
+++ infra/cloudfront.tf
@@ -16,7 +16,30 @@ resource "aws_cloudfront_distribution" "main" {
     origin_access_control_id = aws_cloudfront_origin_access_control.assets.id
   }
 
+  origin {
+    domain_name = aws_lb.alb.dns_name
+    origin_id   = "alb-app"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+
   default_cache_behavior {
+    allowed_methods          = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods           = ["GET", "HEAD"]
+    target_origin_id         = "alb-app"
+    viewer_protocol_policy   = "redirect-to-https"
+    cache_policy_id          = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+    origin_request_policy_id = "216adef6-5c7f-47e4-b989-5492eafa07d3"
+  }
+
+  ordered_cache_behavior {
+    path_pattern           = "/assets/*"
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
     target_origin_id       = "s3-assets"
