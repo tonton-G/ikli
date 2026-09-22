37d6b582fa844b4a03b8d7713f6e3178e3230212
Anthony <83625612+tonton-G@users.noreply.github.com.>
2026-09-22 09:02:18 +0800

    chore: added response and alb listener rule from cdn

--- infra/listeners.tf
+++ infra/listeners.tf
@@ -21,8 +21,31 @@ resource "aws_lb_listener" "https" {
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = aws_acm_certificate_validation.alb.certificate_arn
 
+
   default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Forbidden"
+      status_code  = "403"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "from_cloudfront" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 100
+
+  action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app.arn
+  }
+
+  condition {
+    http_header {
+      http_header_name = "X-Origin-Secret"
+      values           = [random_password.cf_origin_secret.result]
+    }
   }
 }
